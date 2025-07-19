51a4fa19b60c5d1dbf02652de83fd321eb8eff88
Astra3 <rtaborsky02@gmail.com>
2025-07-19 21:22:30 +0200

    adding submodule

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,6 @@
 #include "add.hpp"
 
 int main() {
-    std::cout << "Hello Gabe!" << std::endl;
+    std::cout << "Hello Gabe and me!" << std::endl;
     std::cout << "20 / 2 = " << add(20, 2) << std::endl;
 }
